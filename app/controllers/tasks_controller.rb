7446233d3afdededf6f413e52e9d3901616d7944
T.Tsujii <ttakehisabt@gmail.com>
2020-03-29 17:25:40 +0900

    タスクのCRUDを追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,38 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    Task.create(task_params)
+  end
+
+  def update
+    @task.update(task_params)
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :content)
+    end
+end
