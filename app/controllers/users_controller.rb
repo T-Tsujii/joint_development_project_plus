a089a74d7756e2cb285a66a09c3dd58a44fa4c98
T.Tsujii <ttakehisabt@gmail.com>
2020-03-29 17:25:40 +0900

    ユーザーのCRUDを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  def update
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :age)
+    end
+end
