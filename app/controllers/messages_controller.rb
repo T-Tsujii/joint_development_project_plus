ddbf850ba011fcc0f82a4142e4aca596bec4439f
T.Tsujii <ttakehisabt@gmail.com>
2020-03-30 08:09:46 +0900

    メッセージのCRUDを追加

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,38 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def edit
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+  def update
+    @message.update(message_params)
+  end
+
+  def destroy
+    @message.destroy
+  end
+
+  private
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
+    def message_params
+      params.require(:message).permit(:title, :content)
+    end
+end
